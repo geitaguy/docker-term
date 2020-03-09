034641b42c490ea964fbe64646b33ec5aef01e02
Hamish McKenzie-Campbell <geitaguy@stretch-nas.geitaguy.net>
2020-03-09 22:12:13 +0800

    save exit code to variable

--- root/login.sh
+++ root/login.sh
@@ -5,21 +5,24 @@ export APPDIR=/app
 
 cd ${APPDIR}
 
-if [ -f app.sh ]; then
+if [ -f ${APPDIR}/app.sh ]; then
   ./app.sh
+  exitcode=$?
 else
   echo "Cannot find ${APPDIR}/app.sh"
   read -p "Press <enter> to continue"
+  exitcode=1
+fi
 
 #clear
-if [ $? = 0 ]; then
+if [ ${exitcode} = 0 ]; then
   echo "Program completed successfully."
   echo "Exiting in 5 seconds."
   sleep 5s
   #very important to have an exit line, so the user isn't retured to a bash shell
   exit 0
 else
-  echo "Program completed with exit code $?"
+  echo "Program completed with exit code ${exitcode}"
   read -p "Press <enter> to exit"
-  exit $?
+  exit ${exitcode}
 fi
