c5e9a07ef80586b687d42bd2efba90fd401374f5
Hamish McKenzie-Campbell <geitaguy@stretch-nas.geitaguy.net>
2020-03-09 22:01:16 +0800

    Bailout if no app.sh

--- root/init.sh
+++ root/init.sh
@@ -3,5 +3,5 @@
 #launch telnet and SSH daemons
 echo "Container started. Listening for incoming telnet and ssh connections..."
 service xinetd restart
-/usr/sbin/sshd -D
+#/usr/sbin/sshd -D
 while :; do sleep 1; done
--- root/login.sh
+++ root/login.sh
@@ -4,10 +4,22 @@ export HOME=/root
 export APPDIR=/app
 
 cd ${APPDIR}
-./app.sh
+
+if [ -f app.sh ]; then
+  ./app.sh
+else
+  echo "Cannot find ${APPDIR}/app.sh"
+  read -p "Press <enter> to continue"
 
 #clear
-echo "Program completed. Exiting in 5 seconds..."
-sleep 5s
-#very important to have an exit line, so the user isn't retured to a bash shell
-exit 0
+if [ $? = 0 ]; then
+  echo "Program completed successfully."
+  echo "Exiting in 5 seconds."
+  sleep 5s
+  #very important to have an exit line, so the user isn't retured to a bash shell
+  exit 0
+else
+  echo "Program completed with exit code $?"
+  read -p "Press <enter> to exit"
+  exit $?
+fi
